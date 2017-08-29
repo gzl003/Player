b6a5c85e72b1026f590cc90b4cc595840d487052
zhiguang.li <zhiguang.li@huanxi.fm>
2017-08-29 11:40:20 +0800

    sdas

--- app/src/main/java/com/lzg/palyer/MainActivity.java
+++ app/src/main/java/com/lzg/palyer/MainActivity.java
@@ -3,6 +3,9 @@ package com.lzg.palyer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
